9b38228b0b8283bd315791d1c58df02148468d4c
DaryllOoi <daryllooi@hotmail.com>
2022-11-28 14:53:55 +0000

    homework_day6

---
+++ homework.js
@@ -0,0 +1,17 @@
+console.log("day6");
+
+let name = "daryll";
+let surname = " ooi";
+let middleName = "wen feng";
+
+let fullName;
+fullName = name + surname;
+
+console.log("hello, my name is", fullName);
+
+let age = 24;
+let currentYear = 2022;
+let birthYear = currentYear - age;
+
+console.log("my birth year is :", birthYear);
+
---
+++ index.js
@@ -0,0 +1,54 @@
+let spaghetti = 400;
+let guanciale = 250;
+let veryFreshEggYolks = 6 * 18;
+let agedGratedPecorinoRomanoCheese = 50;
+let rawBlackPepper = 4;
+
+console.log("spaghetti 400g");
+console.log("guanciale 250g");
+console.log("very fresh egg yolks 6");
+console.log("aged grated Pecorino Romano cheese 50g");
+console.log("raw black pepper 4g\n");
+
+console.log("To make classic carbonara, first cut the guanciale into 1cm layers, then into long strips.\n");
+
+let combine;
+combine = veryFreshEggYolks + agedGratedPecorinoRomanoCheese;
+console.log("Combine the egg yolks with the finely grated Pecorino Romano.\n", combine);
+
+let roast;
+roast = rawBlackPepper;
+
+let smallAmount = 50;
+let bigAmount = 108;
+
+let mixture;
+mixture = smallAmount + roast;
+console.log("Roast the black pepper on a pan until it smokes and sizzles, and combine a small amount of it to the yolks and chees mixture, set aside the rest.\n", mixture);
+
+console.log("Brown the strips of guanciale for 3 minutes at medium heat, then 1 minune on high heat til crisp, then turn off the heat, take out the guanciale, leaving only the grease on the pan cool down.", guanciale);
+console.log("Cook the pasta with a pinch of salt in the water (guanciale is already very salty);", spaghetti);
+
+let pastaWater = 10;
+let spoonWater = pastaWater + guanciale;
+console.log("set aside a ladleful of the pasta water before draining it, then drain the pasta once it is cooked al dente.", pastaWater);
+console.log("Pour 2 spoons of the reserved hot water into the part of the frying pan containing the cooled guanciale and turn on the heat:", spoonWater);
+console.log("this will create a creamy sauce with the pasta starch contained in that water.\n", spoonWater);
+console.log("TURN OFF THE HEAT AND SET ASIDE for 1 minute, \n");
+
+spaghetti += spoonWater;
+
+console.log("then transfer the pasta to the same pan and mix together.\n", spaghetti);
+
+spaghetti += bigAmount;
+
+console.log("Add the yolk and cheese mixture, stirring rapidly. \n", spaghetti);
+console.log("In the warm pan with the hot pasta, the eggs will cook gently and become creamy.\n");
+console.log("It is important to do this part quickly to prevent the yolks from congealing and taking on the texture of scrambled eggs.\n")
+
+spaghetti += mixture;
+
+let carbonara
+carbonara = spaghetti;
+
+console.log("And...there you go! A lovely carbonara!", spaghetti)
